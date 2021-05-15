c60f46baa05dfea39f7a37b7ea5bca0f67ba0661
sukantahui <huisukanta77@gmail.com>
2021-05-15 18:28:45 +0530

    uuid is working

--- matka_api/app/Models/PlayMaster.php
+++ matka_api/app/Models/PlayMaster.php
@@ -4,21 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
 
 class PlayMaster extends Model
 {
     use HasFactory;
 
-    /**
-     * @var mixed
-     */
-    private $barcode_number;
-    /**
-     * @var mixed
-     */
+
     private $draw_master_id;
     /**
      * @var mixed
      */
     private $terminal_id;
+    protected static function boot()
+    {
+        parent::boot();
+        static::saving(function ($model) {
+            $model->barcode_number =  (string)Uuid::generate();
+        });
+    }
 }
