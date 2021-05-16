2b4e4da78a837e0d25e52108540d707a809dde4c
sreeparnadas <sreeparnadas0675@gmail.com>
2021-05-16 13:49:04 +0530

    get number combination by single

--- matka_api/app/Models/SingleNumber.php
+++ matka_api/app/Models/SingleNumber.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class SingleNumber extends Model
 {
     use HasFactory;
+
+    public function number_combinations(){
+        return $this->hasMany(NumberCombination::class,'single_number_id');
+    }
 }
