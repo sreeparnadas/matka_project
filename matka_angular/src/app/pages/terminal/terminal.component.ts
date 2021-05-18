7981714a281fe21e8fb58290326a1b94f1ba1b46
sreeparnadas <sreeparnadas0675@gmail.com>
2021-05-18 10:45:00 +0530

    number combinations for each single number fetched and put into separate variables done

--- matka_angular/src/app/pages/terminal/terminal.component.ts
+++ matka_angular/src/app/pages/terminal/terminal.component.ts
@@ -15,7 +15,18 @@ export class TerminalComponent implements OnInit {
   showDeveloperDiv = true;
 
   singleNumbers: SingleNumber[] = [];
+
   numberCombinations: NumberCombinations[] = [];
+  numberCombinationsForOne: NumberCombinations[] = [];
+  numberCombinationsForTwo: NumberCombinations[] = [];
+  numberCombinationsForThree: NumberCombinations[] = [];
+  numberCombinationsForFour: NumberCombinations[] = [];
+  numberCombinationsForFive: NumberCombinations[] = [];
+  numberCombinationsForSix: NumberCombinations[] = [];
+  numberCombinationsForSeven: NumberCombinations[] = [];
+  numberCombinationsForEight: NumberCombinations[] = [];
+  numberCombinationsForNine: NumberCombinations[] = [];
+
   columnNumber = 5;
   constructor(private playGameService: PlayGameService, private commonService: CommonService) { }
 
@@ -29,6 +40,61 @@ export class TerminalComponent implements OnInit {
     this.numberCombinations = this.playGameService.getAllTripleNumbers();
     this.playGameService.getAllTripleNumberListener().subscribe((response: NumberCombinations[]) => {
       this.numberCombinations = response;
+      console.log(this.numberCombinations);
+    });
+
+    // single 1 => triple(22)
+    this.numberCombinationsForOne = this.playGameService.getNumberCombinationsForOne();
+    this.playGameService.getNumberCombinationsForOneListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForOne = response;
+    });
+
+    // single 2 => triple(22)
+    this.numberCombinationsForTwo = this.playGameService.getNumberCombinationsForTwo();
+    this.playGameService.getNumberCombinationsForTwoListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForTwo = response;
+    });
+
+    // single 3 => triple(22)
+    this.numberCombinationsForThree = this.playGameService.getNumberCombinationsForThree();
+    this.playGameService.getNumberCombinationsForThreeListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForThree = response;
+    });
+
+    // single 4 => triple(22)
+    this.numberCombinationsForFour = this.playGameService.getNumberCombinationsForFour();
+    this.playGameService.getNumberCombinationsForFourListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForFour = response;
+    });
+
+    // single 5 => triple(22)
+    this.numberCombinationsForFive = this.playGameService.getNumberCombinationsForFive();
+    this.playGameService.getNumberCombinationsForFiveListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForFive = response;
+    });
+
+    // single 6 => triple(22)
+    this.numberCombinationsForSix = this.playGameService.getNumberCombinationsForSix();
+    this.playGameService.getNumberCombinationsForSixListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForSix = response;
+    });
+
+    // single 7 => triple(22)
+    this.numberCombinationsForSeven = this.playGameService.getNumberCombinationsForSeven();
+    this.playGameService.getNumberCombinationsForSevenListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForSeven = response;
+    });
+
+    // single 8 => triple(22)
+    this.numberCombinationsForEight = this.playGameService.getNumberCombinationsForEight();
+    this.playGameService.getNumberCombinationsForEightListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForEight = response;
+    });
+
+    // single 9 => triple(22)
+    this.numberCombinationsForNine = this.playGameService.getNumberCombinationsForNine();
+    this.playGameService.getNumberCombinationsForNineListener().subscribe((response: NumberCombinations[]) => {
+      this.numberCombinationsForNine = response;
     });
 
     // variableSettings enabling
--- matka_angular/src/app/services/play-game.service.ts
+++ matka_angular/src/app/services/play-game.service.ts
@@ -16,7 +16,26 @@ export class PlayGameService {
   singleNumberSubject = new Subject<SingleNumber[]>();
 
   numberCombinations: NumberCombinations[] = [];
+  numberCombinationsForOne: NumberCombinations[] = [];
+  numberCombinationsForTwo: NumberCombinations[] = [];
+  numberCombinationsForThree: NumberCombinations[] = [];
+  numberCombinationsForFour: NumberCombinations[] = [];
+  numberCombinationsForFive: NumberCombinations[] = [];
+  numberCombinationsForSix: NumberCombinations[] = [];
+  numberCombinationsForSeven: NumberCombinations[] = [];
+  numberCombinationsForEight: NumberCombinations[] = [];
+  numberCombinationsForNine: NumberCombinations[] = [];
+
   numberCombinationsSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForOneSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForTwoSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForThreeSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForFourSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForFiveSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForSixSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForSevenSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForEightSubject = new Subject<NumberCombinations[]>();
+  numberCombinationsForNineSubject = new Subject<NumberCombinations[]>();
 
   constructor(private http: HttpClient) {
     // get single numbers
@@ -28,7 +47,52 @@ export class PlayGameService {
       this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/1').subscribe((response: ServerResponse) => {
         this.numberCombinations = response.data;
         this.numberCombinationsSubject.next([...this.numberCombinations]);
-    });
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/2').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForOne = response.data;
+        this.numberCombinationsForOneSubject.next([...this.numberCombinationsForOne]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/3').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForTwo = response.data;
+        this.numberCombinationsForTwoSubject.next([...this.numberCombinationsForTwo]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/4').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForThree = response.data;
+        this.numberCombinationsForThreeSubject.next([...this.numberCombinationsForThree]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/5').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForFour = response.data;
+        this.numberCombinationsForFourSubject.next([...this.numberCombinationsForFour]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/6').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForFive = response.data;
+        this.numberCombinationsForFiveSubject.next([...this.numberCombinationsForFive]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/7').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForSix = response.data;
+        this.numberCombinationsForSixSubject.next([...this.numberCombinationsForSix]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/8').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForSeven = response.data;
+        this.numberCombinationsForSevenSubject.next([...this.numberCombinationsForSeven]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/9').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForEight = response.data;
+        this.numberCombinationsForEightSubject.next([...this.numberCombinationsForEight]);
+      });
+
+      this.http.get(GlobalVariable.BASE_API_URL + '/numberCombinations/number/10').subscribe((response: ServerResponse) => {
+        this.numberCombinationsForNine = response.data;
+        this.numberCombinationsForNineSubject.next([...this.numberCombinationsForNine]);
+      });
   }
 
   getSingleNumbers(){
@@ -38,12 +102,84 @@ export class PlayGameService {
     return this.singleNumberSubject.asObservable();
   }
 
-  // get triple numbers list by single number
+  // get triple numbers list by single number 0
   getAllTripleNumbers(){
     return [...this.numberCombinations];
   }
-
   getAllTripleNumberListener(){
     return this.numberCombinationsSubject.asObservable();
   }
+
+  // get triple numbers list by single number 1
+  getNumberCombinationsForOne(){
+    return [...this.numberCombinationsForOne];
+  }
+  getNumberCombinationsForOneListener(){
+    return this.numberCombinationsForOneSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 2
+  getNumberCombinationsForTwo(){
+    return [...this.numberCombinationsForTwo];
+  }
+  getNumberCombinationsForTwoListener(){
+    return this.numberCombinationsForTwoSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 3
+  getNumberCombinationsForThree(){
+    return [...this.numberCombinationsForThree];
+  }
+  getNumberCombinationsForThreeListener(){
+    return this.numberCombinationsForThreeSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 4
+  getNumberCombinationsForFour(){
+    return [...this.numberCombinationsForFour];
+  }
+  getNumberCombinationsForFourListener(){
+    return this.numberCombinationsForFourSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 5
+  getNumberCombinationsForFive(){
+    return [...this.numberCombinationsForFive];
+  }
+  getNumberCombinationsForFiveListener(){
+    return this.numberCombinationsForFiveSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 6
+  getNumberCombinationsForSix(){
+    return [...this.numberCombinationsForSix];
+  }
+  getNumberCombinationsForSixListener(){
+    return this.numberCombinationsForThreeSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 7
+  getNumberCombinationsForSeven(){
+    return [...this.numberCombinationsForSeven];
+  }
+  getNumberCombinationsForSevenListener(){
+    return this.numberCombinationsForSevenSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 8
+  getNumberCombinationsForEight(){
+    return [...this.numberCombinationsForEight];
+  }
+  getNumberCombinationsForEightListener(){
+    return this.numberCombinationsForEightSubject.asObservable();
+  }
+
+  // get triple numbers list by single number 9
+  getNumberCombinationsForNine(){
+    return [...this.numberCombinationsForNine];
+  }
+  getNumberCombinationsForNineListener(){
+    return this.numberCombinationsForNineSubject.asObservable();
+  }
+
 }
