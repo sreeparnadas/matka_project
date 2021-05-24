b421befa99f7edbaba3acb7cd1a73fbc96753728
sukanta hui <huisukanta77@gmail.com>
2021-05-24 08:13:10 +0530

    helper file added

--- matka_api/app/Http/Controllers/GameTypeController.php
+++ matka_api/app/Http/Controllers/GameTypeController.php
@@ -14,7 +14,8 @@ class GameTypeController extends Controller
      */
     public function index()
     {
-        $result = GameType::get();
+//        $result = GameType::get();
+        $result = get_age('1977-05-20');
         return response()->json(['success'=>1,'data'=>$result], 200,[],JSON_NUMERIC_CHECK);
     }
 
