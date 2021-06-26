768a56e5687d54726d7830304992d416b3396dda
mounitbhandari <tataibhandari@gmail.com>
2021-06-26 22:48:25 +0530

    added  background images in chips

---
+++ matka_angular/src/app/services/master-stockist.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+// @ts-ignore
+export class MasterStockistService {
+
+  masterStockistForm: FormGroup;
+  constructor() {
+    this.masterStockistForm = new FormGroup({
+      id : new FormControl(null),
+      stockistName : 
+    });
+   }
+}
