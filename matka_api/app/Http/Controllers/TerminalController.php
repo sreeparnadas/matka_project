dcef35d8305312d0fb090f01f0bea774fbc11eca
mounitbhandari <tataibhandari@gmail.com>
2021-07-19 20:39:51 +0530

    upat terminal

--- matka_api/app/Http/Controllers/TerminalController.php
+++ matka_api/app/Http/Controllers/TerminalController.php
@@ -102,12 +102,14 @@ class TerminalController extends Controller
 
         $terminal = User::findOrFail($terminalId);
         $terminal->user_name = $terminalName;
-        
-        $terminal->id = $stockist_id;
         $terminal->save();
 
-        return response()->json(['success'=>1,'data'=> new TerminalResource($terminal)], 200,[],JSON_NUMERIC_CHECK);
+        $stockistToTerminal = StockistToTerminal::where('terminal_id',$terminalId)->first();
+        $stockistToTerminal->stockist_id = $stockist_id;
+        $stockistToTerminal->save();
 
+        return response()->json(['success'=>1,'data'=> new TerminalResource($terminal)], 200,[],JSON_NUMERIC_CHECK);
+        // return response()->json(['data'=> $stockistToTerminal]);
     }
 
     public function update_balance_to_terminal(Request $request){
