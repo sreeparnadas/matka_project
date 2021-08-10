147910ff3ea5dc0b1286ad7162e9e1b6a20102a1
sreeparnadas <sreeparnadas0675@gmail.com>
2021-08-10 23:54:04 +0530

    result component done

--- matka_angular/src/app/modules/general/cpanel/result/result.module.ts
+++ matka_angular/src/app/modules/general/cpanel/result/result.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ResultRoutingModule } from './result-routing.module';
+import {ResultComponent} from '../../../../pages/cpanel/result/result.component';
 
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     ResultRoutingModule
+  ],
+  exports: [
+    ResultComponent
+  ],
+  declarations: [
+    ResultComponent
   ]
 })
 export class ResultModule { }
