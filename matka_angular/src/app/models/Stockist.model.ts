ed97078b3df1efb43fc9a92cbca44efeab6aea12
mounitbhandari <tataibhandari@gmail.com>
2022-02-23 23:44:09 +0530

    add super stockist name in stockist component

--- matka_angular/src/app/models/Stockist.model.ts
+++ matka_angular/src/app/models/Stockist.model.ts
@@ -5,6 +5,17 @@ export class Stockist{
   userId: number;
   userName: string;
   pin: string;
+  // superStockiestId: number;
+  // superStockistName: {
+  //   id: 5,
+  //   user_name: string,
+  //   email: string,
+  //   mobile1: null,
+  //   user_type_id: number,
+  //   opening_balance: number,
+  //   closing_balance: number,
+  //   commission: number
+  // };
   userTypeName?: string;
   userTypeId: number;
   balance: number;
--- matka_angular/src/app/models/StockistMaster.model.ts
+++ matka_angular/src/app/models/StockistMaster.model.ts
@@ -6,6 +6,17 @@ export class StockistMaster{
     userId: number,
     userName: string,
     pin: string,
+    superStockiestId: number;
+    superStockistName: {
+      id: 5,
+      user_name: string,
+      email: string,
+      mobile1: null,
+      user_type_id: number,
+      opening_balance: number,
+      closing_balance: number,
+      commission: number
+    };
     userTypeName?: string,
     userTypeId: number,
     balance: number
